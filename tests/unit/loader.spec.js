b0fcfc2cef24539bcfb0e585be663ed658442122
Sergiu Mare <mare.sergiu@yahoo.com>
2020-06-29 15:32:47 +0100

    master: testing

---
+++ tests/unit/loader.spec.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils';
+import Loader from '@/components/Loader.vue';
+
+describe('Loader.vue', () => {
+  it('renders props.msg when passed', () => {
+    const wrapper = mount(Loader);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+});
